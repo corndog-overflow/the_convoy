a258b983f0e97a23764958b1223095a23df74d30
Nikhil <40872297+Nikkrish44@users.noreply.github.com>
2025-02-13 16:23:44 -0500

    Update pathplanning.py

--- pathplanning/pathplanning.py
+++ pathplanning/pathplanning.py
@@ -1,22 +1,39 @@
+#!/usr/bin/env python3
+
+
 import rclpy
-from rclpy.node import Node
 
-class PathPlanningNode(Node):
-    def __init__(self):
-        super().__init__('path_planning_node')
-        self.get_logger().info('Path Planning Node has started!')
-        self.timer = self.create_timer(1.0, self.publish_dummy_message)
-    
-    def publish_dummy_message(self):
-        self.get_logger().info('Dummy Path Planning Message: Moving to next waypoint!')
+from turtlebot4_navigation.turtlebot4_navigator import TurtleBot4Directions, TurtleBot4Navigator
 
 
-def main(args=None):
-    rclpy.init(args=args)
-    node = PathPlanningNode()
-    rclpy.spin(node)
-    node.destroy_node()
+def main():
+    rclpy.init()
+
+    navigator = TurtleBot4Navigator()
+
+    # # Start on dock
+    # if not navigator.getDockedStatus():
+    #     navigator.info('Docking before intialising pose')
+    #     navigator.dock()
+
+    # Set initial pose
+    initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH)
+    navigator.setInitialPose(initial_pose)
+
+    # Wait for Nav2
+    navigator.waitUntilNav2Active()
+
+    # Set goal poses
+    goal_pose = navigator.getPoseStamped([-13.0, 9.0], TurtleBot4Directions.EAST)
+
+    # # Undock
+    # navigator.undock()
+
+    # Go to each goal pose
+    navigator.startToPose(goal_pose)
+
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
